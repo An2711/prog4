de4185daebdf88b759ad80c3d80016a78c6a6b43
Anusha Rapuru <arapuru@unix4.lt.ucsc.edu>
2023-11-30 15:29:30 -0800

    graph is working and started bfs

--- bfs.c
+++ bfs.c
@@ -3,4 +3,21 @@
 #include <stdlib.h>
 
 void bfs(int **graph, size_t num_cities, int start) {
+    int visited[num_cities][num_cities];
+    for(int i = 0; i < num_cities; i++){
+        for(int j = 0; j < num_cities; j++){
+            visited[num_cities][num_cities] = 0;
+        }
+    }
+    Queue* q = createQueue((int)num_cities);
+    enqueue(q, start);
+    while(isEmpty(q) != 0){
+        int currCity = dequeue(q);
+        for(int i = 0; i < num_cities; i++){
+            if((graph[currCity][i] != 0) && (visited[currCity][i] == 0)){
+                
+            }
+        }
+    }
+
 }
--- main.c
+++ main.c
@@ -4,6 +4,38 @@
 #include "bfs.h"
 #include "dijkstra.h"
 
+typedef struct City{
+    int visited;
+} City;
 
 int main() {
+    char fileName[30];
+    int numCities;
+    int links;
+    scanf("%s\n", fileName);
+    FILE *fptr;
+    fptr = fopen(fileName, "r");
+    if(fptr == NULL){
+        printf("ERROR: file not found!\n");
+        exit(0);
+    }
+    fscanf(fptr, "%d\n", &numCities);
+    int graph[numCities][numCities];
+    // initialize the graph to have all values be 0
+    for(int i = 0; i < numCities; i++){
+        for(int j = 0; j < numCities; j++){
+            graph[i][j] = 0;
+        }
+    }
+    fscanf(fptr, "%d\n", &links);
+    for(int i = 0; i < links; i++){
+        int src;
+        int dest;
+        int time;
+        fscanf(fptr, "%d", &src);
+        fscanf(fptr, ",%d\n", &dest);
+        fscanf(fptr, ",%d", &time);
+        graph[src][dest] = time;
+    }
+    
 }
