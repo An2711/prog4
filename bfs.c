50b321f76af8fd6808b9bee4c0c8c236f73567bf
Anusha Rapuru <arapuru@unix4.lt.ucsc.edu>
2023-11-30 17:02:23 -0800

    trying things

--- bfs.c
+++ bfs.c
@@ -3,21 +3,39 @@
 #include <stdlib.h>
 
 void bfs(int **graph, size_t num_cities, int start) {
-    int visited[num_cities][num_cities];
+   // int visited[num_cities][num_cities];
+    int visited[num_cities];
+    int path[num_cities];
+    // integer array to keep track of shorest visited
+
     for(int i = 0; i < num_cities; i++){
-        for(int j = 0; j < num_cities; j++){
-            visited[num_cities][num_cities] = 0;
-        }
+        // for(int j = 0; j < num_cities; j++){
+        //     visited[num_cities][num_cities] = 0;
+
+        // }
+        visited[i] = 0;
+        path[i] = -1;
     }
     Queue* q = createQueue((int)num_cities);
     enqueue(q, start);
-    while(isEmpty(q) != 0){
+    while(isEmpty(q) == 0){
         int currCity = dequeue(q);
+       // printf("%d\n", currCity);
+       // printf("%d\n",graph[currCity][num_cities-1] != 0);
+       // 
         for(int i = 0; i < num_cities; i++){
             if((graph[currCity][i] != 0) && (visited[currCity][i] == 0)){
+                printf("%d\n", graph[currCity][i]);
+                visited[currCity][i] == 1;
+                path[i] = currCity;
+                enqueue(q, i);
+                if(i == n -1)
+
                 
             }
+
         }
     }
+    freeQueue(q);
 
 }
--- main.c
+++ main.c
@@ -4,9 +4,6 @@
 #include "bfs.h"
 #include "dijkstra.h"
 
-typedef struct City{
-    int visited;
-} City;
 
 int main() {
     char fileName[30];
@@ -20,9 +17,12 @@ int main() {
         exit(0);
     }
     fscanf(fptr, "%d\n", &numCities);
-    int graph[numCities][numCities];
+    int** graph;
+    graph = (int**)malloc(sizeof(int*)*numCities);
+    //int graph[numCities][numCities];
     // initialize the graph to have all values be 0
     for(int i = 0; i < numCities; i++){
+        graph[i] = (int*)malloc(numCities*sizeof(int));
         for(int j = 0; j < numCities; j++){
             graph[i][j] = 0;
         }
@@ -37,5 +37,7 @@ int main() {
         fscanf(fptr, ",%d", &time);
         graph[src][dest] = time;
     }
+
+    bfs(graph, numCities, 0);
     
 }
